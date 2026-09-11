cbe4a13fa830c00575c7ae202faecefb6c557520
Timothy <timothytimothytimo6@gmail.com>
2026-09-11 13:21:41 -0400

    fix: update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,18 +5,14 @@
   };
   outputs = { self, nixpkgs, utils, ... }:
     utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            typst
-          ];
-
-          shellHook = ''
-            echo `${pkgs.typst}/bin/typst --version`
-          '';
-        };
-      });
+    let pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          typst
+        ];
+        shellHook = ''echo `${pkgs.typst}/bin/typst --version`'';
+      };
+    });
 }
